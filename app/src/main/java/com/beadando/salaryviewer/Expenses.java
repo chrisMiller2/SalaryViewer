cba57e07f800c8bd961a0d4beb55167633fd1645
Chris_M <mkrisz09@vipmail.hu>
2019-10-29 16:04:38 +0100

    Simple listing and expenses object to handle expense types and costs

---
+++ app/src/main/java/com/beadando/salaryviewer/Expenses.java
@@ -0,0 +1,22 @@
+package com.beadando.salaryviewer;
+
+public class Expenses {
+    String name;
+    int cose;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCose() {
+        return cose;
+    }
+
+    public void setCose(int cose) {
+        this.cose = cose;
+    }
+}
--- app/src/main/java/com/beadando/salaryviewer/MainActivity.java
+++ app/src/main/java/com/beadando/salaryviewer/MainActivity.java
@@ -3,12 +3,61 @@ package com.beadando.salaryviewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> listItems = new ArrayList<String>();
+    ListView listView;
+    EditText expenseText;
+    EditText expenseCost;
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expenseText = findViewById(R.id.inputExpenseID);
+        expenseCost = findViewById(R.id.inputCostID);
+
+        listView = (ListView) findViewById(R.id.listID);
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        listView.setAdapter(adapter);
+    }
+
+    public void updateList(View view) {
+        if(!expenseText.getText().toString().equals(""))
+        {
+            listItems.add(expenseText.getText().toString());
+            adapter.notifyDataSetChanged();
+            Toast.makeText(this, "item added", Toast.LENGTH_LONG).show();
+
+            expenseText.setText("");
+            expenseCost.setText("");
+        }else{
+            Toast.makeText(this, "Can't list nothing", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void removeFromList(View view) {
+        if(listItems.size() >= 1){
+            listItems.remove(listItems.size()-1);
+            adapter.notifyDataSetChanged();
+            Toast.makeText(this, "item removed", Toast.LENGTH_LONG).show();
+            expenseText.setText("");
+            expenseCost.setText("");
+        }else{
+            Toast.makeText(this, "Nothing left to remove", Toast.LENGTH_LONG).show();
+        }
     }
 }
